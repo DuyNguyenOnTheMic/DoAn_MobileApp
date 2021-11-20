30b8be3250bae198c440158aba111d1439df0fbf
DuyNTOnTheMic <duy.197pm21905@vanlanguni.vn>
2021-11-20 17:39:43 +0700

    Update menubar bên quản trị

--- app/src/main/java/com/example/doan_mobile/AdminCategoryActivity.java
+++ app/src/main/java/com/example/doan_mobile/AdminCategoryActivity.java
@@ -1,18 +1,22 @@
 package com.example.doan_mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
 
     ImageView lenovo, acer, asus, hp, apple, dell, msi, lg;
-
-
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,12 @@ public class AdminCategoryActivity extends AppCompatActivity {
 
 
         matching();
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(new HomeFragment());
+
 
         lenovo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,6 +106,39 @@ public class AdminCategoryActivity extends AppCompatActivity {
 
     }
 
+    private void loadFragment(androidx.fragment.app.Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_manage:
+                    fragment = new ManageFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_logout:
+                    Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                    return true;
+            }
+            return false;
+        }
+    };
+
     private void matching() {
         lenovo = (ImageView) findViewById(R.id.adminCategory_iv_lenovo);
         acer = (ImageView) findViewById(R.id.adminCategory_iv_acer);
@@ -105,6 +148,7 @@ public class AdminCategoryActivity extends AppCompatActivity {
         dell = (ImageView) findViewById(R.id.adminCategory_iv_dell);
         msi = (ImageView) findViewById(R.id.adminCategory_iv_msi);
         lg = (ImageView) findViewById(R.id.adminCategory_iv_lg);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
 
     }
 }
