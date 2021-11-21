27224072f473945876fcb5362f64d3c422237219
DuyNTOnTheMic <duy.197pm21905@vanlanguni.vn>
2021-11-21 11:11:58 +0700

    Update Bottom Navigation bên quản trị - ver 1.1

--- app/src/main/java/com/example/doan_mobile/AdminCategoryActivity.java
+++ app/src/main/java/com/example/doan_mobile/AdminCategoryActivity.java
@@ -4,8 +4,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -19,7 +17,6 @@ import io.paperdb.Paper;
 
 public class AdminCategoryActivity extends AppCompatActivity {
 
-    ImageView lenovo, acer, asus, hp, apple, dell, msi, lg;
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -36,77 +33,7 @@ public class AdminCategoryActivity extends AppCompatActivity {
         loadFragment(new ManageFragment());
 
 
-        lenovo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "Lenovo");
-                startActivity(intent);
-            }
-        });
 
-        acer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "Acer");
-                startActivity(intent);
-            }
-        });
-
-        asus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "Asus");
-                startActivity(intent);
-            }
-        });
-
-        hp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "HP");
-                startActivity(intent);
-            }
-        });
-
-        apple.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "Apple");
-                startActivity(intent);
-            }
-        });
-
-        dell.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "Dell");
-                startActivity(intent);
-            }
-        });
-
-        msi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "MSI");
-                startActivity(intent);
-            }
-        });
-
-        lg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
-                intent.putExtra("HangSP", "LG");
-                startActivity(intent);
-            }
-        });
 
     }
 
@@ -163,14 +90,6 @@ public class AdminCategoryActivity extends AppCompatActivity {
     };
 
     private void matching() {
-        lenovo = (ImageView) findViewById(R.id.adminCategory_iv_lenovo);
-        acer = (ImageView) findViewById(R.id.adminCategory_iv_acer);
-        asus = (ImageView) findViewById(R.id.adminCategory_iv_asus);
-        hp = (ImageView) findViewById(R.id.adminCategory_iv_hp);
-        apple = (ImageView) findViewById(R.id.adminCategory_iv_apple);
-        dell = (ImageView) findViewById(R.id.adminCategory_iv_dell);
-        msi = (ImageView) findViewById(R.id.adminCategory_iv_msi);
-        lg = (ImageView) findViewById(R.id.adminCategory_iv_lg);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
 
     }
--- app/src/main/java/com/example/doan_mobile/ManageFragment.java
+++ app/src/main/java/com/example/doan_mobile/ManageFragment.java
@@ -1,41 +1,29 @@
 package com.example.doan_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ManageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ManageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    ImageView lenovo, acer, asus, hp, apple, dell, msi, lg;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public ManageFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ManageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ManageFragment newInstance(String param1, String param2) {
         ManageFragment fragment = new ManageFragment();
         Bundle args = new Bundle();
@@ -51,13 +39,36 @@ public class ManageFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
         }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage, container, false);
+        View view = inflater.inflate(R.layout.fragment_manage, container, false);
+
+        lenovo = (ImageView) view.findViewById(R.id.adminCategory_iv_lenovo);
+        acer = (ImageView) view.findViewById(R.id.adminCategory_iv_acer);
+        asus = (ImageView) view.findViewById(R.id.adminCategory_iv_asus);
+        hp = (ImageView) view.findViewById(R.id.adminCategory_iv_hp);
+        apple = (ImageView) view.findViewById(R.id.adminCategory_iv_apple);
+        dell = (ImageView) view.findViewById(R.id.adminCategory_iv_dell);
+        msi = (ImageView) view.findViewById(R.id.adminCategory_iv_msi);
+        lg = (ImageView) view.findViewById(R.id.adminCategory_iv_lg);
+
+        lenovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AdminAddNewProductActivity.class);
+                intent.putExtra("HangSP", "Lenovo");
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
