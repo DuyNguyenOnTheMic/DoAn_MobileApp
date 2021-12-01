d81a44e03b42c47dbd3cda5239c3e3aad122c5bd
DuyNTOnTheMic <duy.197pm21905@vanlanguni.vn>
2021-12-01 14:54:04 +0700

    Update again

--- app/src/main/java/com/example/doan_mobile/SearchProductsActivity.java
+++ app/src/main/java/com/example/doan_mobile/SearchProductsActivity.java
@@ -48,7 +48,6 @@ public class SearchProductsActivity extends AppCompatActivity {
             }
         });
 
-        // Search bằng bàn phím
         input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
